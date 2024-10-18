20bef86c332d98936b574a78ce9e864521889b95
lucas-cauhe <lcauhe@gmail.com>
2024-10-18 18:43:13 +0200

    rota

--- bin/main.ml
+++ bin/main.ml
@@ -28,10 +28,10 @@ let my_scene : scene = [
 ]
 
 module PpmDb = Computer_gfx.Db.Ppm
-let left = ref (Direction.from_coords 0. (1.) 0.)
-let up = ref (Direction.from_coords (1.) 0. 0.)
-let forward = ref (Direction.from_coords 0. 0. (5.))
-let origin = ref (Point.from_coords 0. 0. (-8.5))
+let left = ref (Direction.from_coords (-2.) (-2.) 0.)
+let up = ref (Direction.from_coords (-2.) 2. 0.)
+let forward = ref (Direction.from_coords 0. 0. 3.)
+let origin = ref (Point.from_coords 0. 0. (-3.5))
 
 
 let () = 
--- lib/camera.ml
+++ lib/camera.ml
@@ -12,14 +12,29 @@ let camera up left forward origin = {
   width = 256; height = 256;
 }
 
-let point_in_pixel cam (row, col) =
+(* let point_in_pixel cam (row, col) =
   let open Geometry in
   let left = Direction.prod cam.left (1. -. float_of_int row /. (float_of_int cam.width /. 2.)) in
   let up = Direction.prod cam.up (1. -. float_of_int col /. (float_of_int cam.height /. 2.)) in
   (* Geometry.Point.from_coords x y ((Point.z cam.origin) +. (Direction.z cam.forward)) *)
   let dir = cam.origin |> Direction.of_point |> Direction.sum cam.forward |> Direction.sum left |> Direction.sum up in
   (* let dir = Direction.sum (Direction.sum (Direction.sum (Direction.of_point cam.origin) cam.forward) left) up in *)
-  Point.from_coords (Direction.x dir)  (Direction.y dir)  (Direction.z dir)
+  Point.from_coords (Direction.x dir)  (Direction.y dir)  (Direction.z dir) *)
+
+let point_in_pixel cam (row, col) = 
+  let open Geometry in
+  let x_ = 2. *. (float_of_int col /. float_of_int cam.width) -. 1. in
+  let y_ = 1. -. 2. *. (float_of_int row /. float_of_int cam.height) in
+  let fov = Float.pi /. 2. in
+  let distance_to_pplane = 1. in
+  let ar = float_of_int cam.width /. float_of_int cam.height in
+  let half_height = tan (fov /. 2.) *. distance_to_pplane in
+  let half_width = half_height *. ar in
+  let left_ = Direction.prod cam.left (x_ *. half_width) in
+  let up_ = Direction.prod cam.up (y_ *. half_height) in
+  let forward_ = Direction.prod cam.forward distance_to_pplane in
+  let dir = Direction.sum left_ up_ |> Direction.sum forward_ in
+  Point.from_coords (Direction.x dir)  (Direction.y dir)  (Direction.z dir) 
 
  
 let trace_ray cam (row, col) scene pool =
