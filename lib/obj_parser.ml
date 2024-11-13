f6ea3f55719bcff0f7f314370304c1bc15fe6e9f
Andreiiii19 <844417@unizar.es>
2024-11-13 12:56:52 +0100

    added obj_parser

---
+++ lib/obj_parser.ml
@@ -0,0 +1,69 @@
+type vertex =
+  { x : float
+  ; y : float
+  ; z : float
+  }
+
+type normal =
+  { nx : float
+  ; ny : float
+  ; nz : float
+  }
+
+type face =
+  { v1 : int
+  ; v2 : int
+  ; v3 : int
+  }
+
+let parse_vertex line = Scanf.sscanf line "v %f %f %f" (fun x y z -> { x; y; z })
+let parse_normal line = Scanf.sscanf line "vn %f %f %f" (fun nx ny nz -> { nx; ny; nz })
+
+let parse_face line =
+  Scanf.sscanf line "f %d/%d/%d %d/%d/%d %d/%d/%d" (fun v1 _ _ v2 _ _ v3 _ _ ->
+    { v1; v2; v3 })
+;;
+
+let read_obj_file filename =
+  let vertices = ref [] in
+  let normals = ref [] in
+  let faces = ref [] in
+  let ic = open_in filename in
+  try
+    while true do
+      let line = input_line ic in
+      if String.length line >= 2 then (
+        match String.sub line 0 2 with
+        | "v " -> vertices := parse_vertex line :: !vertices
+        | "vn" -> normals := parse_normal line :: !normals
+        | "f " -> faces := parse_face line :: !faces
+        | _ -> ()
+      )
+    done;
+    !vertices, !normals, !faces
+  with
+  | End_of_file ->
+    close_in ic;
+    !vertices, !normals, !faces
+;;
+
+let convert_to_scene (vertexs, faces) =
+  let convert_vertex v = Geometry.Point.from_coords v.x v.y v.z in
+  let convert_face f =
+    let v1 = List.nth vertexs (f.v1 - 1) in
+    let v2 = List.nth vertexs (f.v2 - 1) in
+    let v3 = List.nth vertexs (f.v3 - 1) in
+    Scene.Figures.Figure
+      (Scene.Figures.triangle
+         (convert_vertex v1)
+         (convert_vertex v2)
+         (convert_vertex v3)
+         (Colorspace.Rgb.rgb_of_values 0.75 0.75 0.)
+         ~coefficients:
+           ( Colorspace.Rgb.rgb_of_values 0.75 0.75 0.
+           , Colorspace.Rgb.zero ()
+           , Colorspace.Rgb.zero () )
+       |> Option.get)
+  in
+  List.map convert_face faces
+;;
